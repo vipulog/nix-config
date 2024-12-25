e22c3c76f90e937d731b3441fa6b25bcaa0e62dd
VipulOG <90324465+VipulOG@users.noreply.github.com>
2024-12-25 10:56:36 +0530

    update flake

--- flake.nix
+++ flake.nix
@@ -19,12 +19,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixvim = {
+    #   url = "github:nix-community/nixvim";
+    #   # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    #   # url = "github:nix-community/nixvim/nixos-24.05";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = inputs@{ self, ... }:
