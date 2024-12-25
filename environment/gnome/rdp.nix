6fdb5e3986a06954822fd6313931b86c2c75c099
VipulOG <90324465+VipulOG@users.noreply.github.com>
2024-12-25 14:47:52 +0530

    Fix rdp
    Fix black screen and disconnection after login issue.

--- environment/gnome/rdp.nix
+++ environment/gnome/rdp.nix
@@ -7,4 +7,11 @@
   services.xrdp.enable = true;
   services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
   services.xrdp.openFirewall = true;
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
