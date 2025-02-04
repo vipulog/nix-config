2ce6ab4002a8fe427664e799ae2e9dc21a1671ae
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-04 17:34:30 +0530

    Change btop clock format

--- modules/home/vipul/optional/btop.nix
+++ modules/home/vipul/optional/btop.nix
@@ -7,7 +7,7 @@
       rounded_corners = false;
       shown_boxes = "proc cpu gpu0 mem net";
       proc_tree = true;
-      clock_format = "/username@/host %X";
+      clock_format = "/username@/host %X - %A, %d %B %Y";
     };
   };
 }
