cd2cd87fa1be3e5d872947cc8ba8ad03f32a0370
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-04 17:35:58 +0530

    Change transition

--- modules/home/vipul/optional/desktop/niri/wpaperd.nix
+++ modules/home/vipul/optional/desktop/niri/wpaperd.nix
@@ -6,10 +6,7 @@
         path = "/home/vipul/Pictures/Wallpapers/";
         sorting = "random";
         duration = "10m";
-        transition-time = 3000;
-        transition.linear-blur = {
-          intensity = "0.5";
-        };
+        transition.colour-distance = {};
       };
     };
   };
