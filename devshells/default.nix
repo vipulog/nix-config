8dec6b153365ec22ba4712980387a70053f5e605
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-04 14:52:48 +0530

    Add helix, nil, nixfmt to devshell

--- devshells/default.nix
+++ devshells/default.nix
@@ -6,5 +6,11 @@
   default = pkgs.mkShell {
     inherit (checks.pre-commit-check) shellHook;
     buildInputs = checks.pre-commit-check.enabledPackages;
+
+    packages = with pkgs; [
+      helix
+      nil
+      nixfmt
+    ];
   };
 }
