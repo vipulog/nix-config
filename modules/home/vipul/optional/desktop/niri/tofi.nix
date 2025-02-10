da1b91ff9ca958a28f29602bfddd81b046c263f6
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-10 22:20:22 +0530

    Fix tofi cache and update some configs

--- modules/home/vipul/optional/desktop/niri/tofi.nix
+++ modules/home/vipul/optional/desktop/niri/tofi.nix
@@ -1,8 +1,17 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }: {
+  home.activation = {
+    # https://github.com/philj56/tofi/issues/115#issuecomment-1701748297
+    regenerateTofiCache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      tofi_cache=${config.xdg.cacheHome}/tofi-drun
+      [[ -f "$tofi_cache" ]] && rm "$tofi_cache"
+    '';
+  };
+
   programs.tofi = {
     enable = true;
 
@@ -14,22 +23,22 @@
       terminal = "${lib.getExe pkgs.ghostty} -e";
       anchor = "bottom";
       width = "100%";
-      height = 46;
+      height = 36;
       horizontal = true;
-      font-size = 13;
+      font-size = 12;
       prompt-text = " run: ";
       outline-width = 0;
-      border-width = 4;
+      border-width = 2;
       border-color = "#7fc8ff";
       selection-color = "#ffffff";
       default-result-color = "#7c7c7c";
       background-color = "#000000";
-      min-input-width = 8;
-      result-spacing = 42;
-      padding-top = 8;
-      padding-bottom = 8;
-      padding-left = 8;
-      padding-right = 8;
+      min-input-width = 20;
+      result-spacing = 20;
+      padding-top = 4;
+      padding-bottom = 4;
+      padding-left = 4;
+      padding-right = 4;
     };
   };
 }
