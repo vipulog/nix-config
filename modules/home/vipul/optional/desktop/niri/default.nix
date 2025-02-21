0889e9da7bb40d4c11c88c22e38681fa8dd73fe3
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-21 16:45:59 +0530

    Update keybindings

--- modules/home/vipul/optional/desktop/niri/default.nix
+++ modules/home/vipul/optional/desktop/niri/default.nix
@@ -62,87 +62,89 @@ in {
       sh = spawn "sh" "-c";
     in
       lib.attrsets.mergeAttrsList [
+        # Workspace Navigation
         {
-          "Mod+1".action = focus-workspace 1;
-          "Mod+2".action = focus-workspace 2;
-          "Mod+3".action = focus-workspace 3;
-          "Mod+4".action = focus-workspace 4;
-          "Mod+5".action = focus-workspace 5;
-          "Mod+6".action = focus-workspace 6;
-          "Mod+7".action = focus-workspace 7;
-          "Mod+8".action = focus-workspace 8;
-          "Mod+9".action = focus-workspace 9;
-
-          "Mod+Shift+1".action = move-column-to-workspace 1;
-          "Mod+Shift+2".action = move-column-to-workspace 2;
-          "Mod+Shift+3".action = move-column-to-workspace 3;
-          "Mod+Shift+4".action = move-column-to-workspace 4;
-          "Mod+Shift+5".action = move-column-to-workspace 5;
-          "Mod+Shift+6".action = move-column-to-workspace 6;
-          "Mod+Shift+7".action = move-column-to-workspace 7;
-          "Mod+Shift+8".action = move-column-to-workspace 8;
-          "Mod+Shift+9".action = move-column-to-workspace 9;
+          "Mod+U".action = focus-workspace-down;
+          "Mod+I".action = focus-workspace-up;
+          "Mod+Ctrl+U".action = move-column-to-workspace-down;
+          "Mod+Ctrl+I".action = move-column-to-workspace-up;
+          "Mod+Shift+U".action = move-workspace-down;
+          "Mod+Shift+I".action = move-workspace-up;
         }
-        {
-          "Mod+Comma".action = consume-window-into-column;
-          "Mod+Period".action = expel-window-from-column;
 
-          "Mod+Shift+R".action = switch-preset-window-height;
+        # Monitor Navigation
+        {
+          "Mod+Shift+L".action = focus-monitor-right;
+          "Mod+Shift+H".action = focus-monitor-left;
+          "Mod+Shift+J".action = focus-monitor-down;
+          "Mod+Shift+K".action = focus-monitor-up;
+          "Mod+Ctrl+Shift+L".action = move-column-to-monitor-right;
+          "Mod+Ctrl+Shift+H".action = move-column-to-monitor-left;
+          "Mod+Ctrl+Shift+J".action = move-column-to-monitor-down;
+          "Mod+Ctrl+Shift+K".action = move-column-to-monitor-up;
+        }
+
+        # Column Management
+        {
           "Mod+R".action = switch-preset-column-width;
-          "Mod+Ctrl+R".action = reset-window-height;
-          "Mod+Shift+F".action = fullscreen-window;
-          "Mod+Backspace".action = close-window;
-          "Mod+Q".action = close-window;
+
           "Mod+F".action = maximize-column;
           "Mod+C".action = center-column;
-          "Mod+Escape".action = switch-focus-between-floating-and-tiling;
-          "Mod+Ctrl+Escape".action = toggle-window-floating;
 
-          "Mod+Shift+Right".action = move-column-right;
-          "Mod+Shift+Left".action = move-column-left;
-          "Mod+Right".action = focus-column-right;
-          "Mod+Left".action = focus-column-left;
-
-          "Mod+Shift+L".action = move-column-right;
-          "Mod+Shift+H".action = move-column-left;
           "Mod+L".action = focus-column-right;
           "Mod+H".action = focus-column-left;
+          "Mod+Ctrl+L".action = move-column-right;
+          "Mod+Ctrl+H".action = move-column-left;
 
-          "Mod+Shift+Down".action = move-window-down-or-to-workspace-down;
-          "Mod+Shift+Up".action = move-window-up-or-to-workspace-up;
-          "Mod+Down".action = focus-window-or-workspace-down;
-          "Mod+Up".action = focus-window-or-workspace-up;
-
-          "Mod+Shift+J".action = move-window-down-or-to-workspace-down;
-          "Mod+Shift+K".action = move-window-up-or-to-workspace-up;
-          "Mod+J".action = focus-window-or-workspace-down;
-          "Mod+K".action = focus-window-or-workspace-up;
-
-          "Mod+Shift+Tab".action = focus-window-up-or-column-left;
-          "Mod+Tab".action = focus-window-down-or-column-right;
-
-          "Mod+Shift+Minus".action = set-window-height "-10%";
-          "Mod+Shift+Equal".action = set-window-height "+10%";
           "Mod+Minus".action = set-column-width "-10%";
           "Mod+Equal".action = set-column-width "+10%";
         }
+
+        # Window Management
         {
+          "Mod+Shift+R".action = switch-preset-window-height;
+          "Mod+Ctrl+R".action = reset-window-height;
+
+          "Mod+Shift+F".action = fullscreen-window;
+          "Mod+Q".action = close-window;
+
+          "Mod+J".action = focus-window-down;
+          "Mod+K".action = focus-window-up;
+          "Mod+Ctrl+J".action = move-window-down;
+          "Mod+Ctrl+K".action = move-window-up;
+
+          "Mod+Shift+Minus".action = set-window-height "-10%";
+          "Mod+Shift+Equal".action = set-window-height "+10%";
+
+          "Mod+Comma".action = consume-window-into-column;
+          "Mod+Period".action = expel-window-from-column;
+          "Mod+BracketLeft".action = consume-or-expel-window-left;
+          "Mod+BracketRight".action = consume-or-expel-window-right;
+
+          "Mod+V".action = toggle-window-floating;
+          "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
+        }
+
+        # Screenshots
+        {
+          "Print".action = screenshot;
           "Mod+Print".action = screenshot-window;
-          "Print".action = screenshot-screen;
-          "Mod+Shift+S".action = screenshot;
+          "Mod+Shift+Print".action = screenshot-screen;
         }
+
+        # Application Launchers
         {
-          "Mod+V".action = sh "${getExe pkgs.ghostty} -e '${getExe pkgs.clipse}'";
-          "Mod+Space".action = sh "${getExe' pkgs.tofi "tofi-drun"} | xargs niri msg action spawn --";
-          "Alt+Space".action = sh "if pidof waybar > /dev/null; then kill $(pidof waybar); else waybar; fi";
-          "Mod+Shift+Space".action = sh "${getExe' pkgs.tofi "tofi-run"} | xargs niri msg action spawn --";
-          "Mod+Return".action = spawn "${getExe pkgs.ghostty}";
-          "Mod+Period".action = spawn "${getExe bemoji-tofi}";
+          "Mod+T".action = spawn "${getExe pkgs.ghostty}";
+          "Mod+D".action = sh "${getExe' pkgs.tofi "tofi-drun"} | xargs niri msg action spawn --";
+          "Mod+W".action = sh "if pidof waybar > /dev/null; then kill $(pidof waybar); else waybar; fi";
+          "Mod+E".action = spawn "${getExe bemoji-tofi}";
+          "Mod+Shift+P".action = spawn "${getExe power-tofi}";
         }
+
+        # System Actions
         {
-          "Mod+Shift+Backspace".action = quit;
-          "Mod+Shift+Q".action = quit;
-          "Mod+F4".action = spawn "${getExe power-tofi}";
+          "Mod+Shift+E".action = quit;
+          "Ctrl+Alt+Delete".action = quit;
           "Mod+Shift+Slash".action = show-hotkey-overlay;
         }
       ];
