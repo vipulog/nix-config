6087ca368cd9d5f867140380996676fcf21fc4ea
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-23 18:35:09 +0530

    make autoLogin.username mandatory

--- modules/nixos/optional/greetd.nix
+++ modules/nixos/optional/greetd.nix
@@ -2,25 +2,25 @@
   config,
   lib,
   ...
-}: {
+}: let
+  autoLoginCfg = config.autoLogin;
+in {
   options.autoLogin = {
     enable = lib.mkEnableOption "Enable automatic login";
 
     username = lib.mkOption {
       type = lib.types.str;
-      default = "guest";
       description = "User to automatically login";
+      example = "john_doe";
     };
   };
 
-  config = {
-    services.greetd = {
-      enable = true;
+  config.services.greetd = {
+    enable = true;
 
-      settings = {
-        initial_session = lib.mkIf config.autoLogin.enable {
-          user = "${config.autoLogin.username}";
-        };
+    settings = {
+      initial_session = lib.mkIf autoLoginCfg.enable {
+        user = "${autoLoginCfg.username}";
       };
     };
   };
