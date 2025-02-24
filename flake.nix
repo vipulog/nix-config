e88b31f19b44037cad3abfb5750a4897780576f1
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-02-24 10:06:56 +0530

    Move inputs above outputs

--- flake.nix
+++ flake.nix
@@ -1,49 +1,6 @@
 {
   description = "Vipul's nixos configuration";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    lib = nixpkgs.lib.extend (_self: _super: {
-      custom = import ./lib.nix {inherit (nixpkgs) lib;};
-    });
-
-    forAllSystems = lib.genAttrs ["x86_64-linux"];
-
-    hostsDir = ./hosts;
-    hostNames = builtins.attrNames (builtins.readDir hostsDir);
-
-    mkHostConfig = host:
-      lib.nixosSystem {
-        modules = [(hostsDir + "/${host}/configuration.nix")];
-        specialArgs = {inherit inputs host lib;};
-      };
-  in {
-    nixosConfigurations = lib.genAttrs hostNames mkHostConfig;
-
-    devShells = forAllSystems (system:
-      import ./devshells {
-        inherit inputs system;
-        pkgs = nixpkgs.legacyPackages.${system};
-        checks = self.checks.${system};
-      });
-
-    checks = forAllSystems (system:
-      import ./checks {
-        inherit inputs system;
-        pkgs = nixpkgs.legacyPackages.${system};
-        treefmt = self.formatter.${system};
-      });
-
-    formatter = forAllSystems (system:
-      import ./treefmt.nix {
-        inherit inputs system;
-        pkgs = nixpkgs.legacyPackages.${system};
-      });
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -85,5 +42,48 @@
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib.extend (_self: _super: {
+      custom = import ./lib.nix {inherit (nixpkgs) lib;};
+    });
+
+    forAllSystems = lib.genAttrs ["x86_64-linux"];
+
+    hostsDir = ./hosts;
+    hostNames = builtins.attrNames (builtins.readDir hostsDir);
+
+    mkHostConfig = host:
+      lib.nixosSystem {
+        modules = [(hostsDir + "/${host}/configuration.nix")];
+        specialArgs = {inherit inputs host lib;};
+      };
+  in {
+    nixosConfigurations = lib.genAttrs hostNames mkHostConfig;
+
+    devShells = forAllSystems (system:
+      import ./devshells {
+        inherit inputs system;
+        pkgs = nixpkgs.legacyPackages.${system};
+        checks = self.checks.${system};
+      });
+
+    checks = forAllSystems (system:
+      import ./checks {
+        inherit inputs system;
+        pkgs = nixpkgs.legacyPackages.${system};
+        treefmt = self.formatter.${system};
+      });
+
+    formatter = forAllSystems (system:
+      import ./treefmt.nix {
+        inherit inputs system;
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
   };
 }
