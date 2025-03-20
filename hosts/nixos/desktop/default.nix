9f95905be861727c3af8413892d4ef252c4437bd
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-03-20 18:53:52 +0530

    feat(desktop): Add vipul user to the networkmanager group
    Adds the "vipul" user to the `internal.services.networking.users` list,
    granting them access to NetworkManager functionality. This allows the
    user to manage network connections.

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -7,6 +7,7 @@
   config = {
     internal = {
       roles.workstation.enable = true;
+      services.networking.users = ["vipul"];
     };
 
     # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
