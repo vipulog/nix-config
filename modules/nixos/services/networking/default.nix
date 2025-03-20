65768db7e2b23373a0291cf372762f940475ac3b
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-03-20 18:51:37 +0530

    feat(networking): Add `users` option to manage NetworkManager group membership
    Introduces the `internal.services.networking.users` option, which takes
    a list of usernames and adds them to the `networkmanager` group,
    granting them permission to use NetworkManager.

--- modules/nixos/services/networking/default.nix
+++ modules/nixos/services/networking/default.nix
@@ -5,12 +5,25 @@
 }:
 with lib; let
   cfg = config.internal.services.networking;
+  userAttrs = listToAttrs (map addGroup cfg.users);
+
+  addGroup = user: {
+    name = user;
+    value = {extraGroups = ["networkmanager"];};
+  };
 in {
   options.internal.services.networking = {
     enable = mkEnableOption "networking";
+
+    users = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "List of users to add to the networkmanager group.";
+    };
   };
 
   config = mkIf cfg.enable {
     networking.networkmanager.enable = true;
+    users.users = userAttrs;
   };
 }
