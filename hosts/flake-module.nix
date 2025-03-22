26917939cd5f75c13693c2875f63755c7ceb6239
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-03-22 10:59:05 +0530

    feat(hosts): Restructure host configurations for multi-system support
    Refactors the host configuration system to support multiple systems
    (architectures) under both NixOS and Nix-on-Droid. This includes moving
    host configurations to system-specific directories and updating the
    flake module to generate configurations based on system architecture.

--- hosts/flake-module.nix
+++ hosts/flake-module.nix
@@ -1,44 +1,89 @@
 {
+  withSystem,
   inputs,
   self,
   lib,
   ...
-}: let
-  nixosHostsDir = ./nixos;
-  nixOnDroidHostsDir = ./nix-on-droid;
+}:
+with lib; let
+  mkNixosConfig = system: host:
+    withSystem system (ctx:
+      inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit (ctx) inputs' self';
+          inherit inputs self;
+          packages = ctx.config.packages or {};
+          pkgsStable = pkgsStable system;
+          pkgsUnstable = pkgsUnstable system;
+        };
+        modules = [
+          self.nixosModules.default
+          "${platformDirs.nixos}/${system}/${host}"
+        ];
+      });
 
-  nixosHosts = lib.pipe (builtins.readDir nixosHostsDir) [
-    (lib.filterAttrs (_name: type: type == "directory"))
-    builtins.attrNames
-  ];
+  mkNixOnDroidConfig = system: host:
+    withSystem system (ctx:
+      inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+        specialArgs = {
+          inherit (ctx) inputs' self';
+          inherit inputs self;
+          packages = ctx.config.packages or {};
+          pkgsStable = pkgsStable system;
+          pkgsUnstable = pkgsUnstable system;
+        };
+        modules = [
+          self.nixOnDroidModules.default
+          "${platformDirs.nixOnDroid}/${system}/${host}"
+        ];
+      });
 
-  nixOnDroidHosts = lib.pipe (builtins.readDir nixOnDroidHostsDir) [
-    (lib.filterAttrs (_name: type: type == "directory"))
-    builtins.attrNames
-  ];
+  # Directory paths mapped to platform names
+  platformDirs = {
+    nixos = ./nixos;
+    nixOnDroid = ./nix-on-droid;
+  };
 
-  mkNixosHostConfig = hostName:
-    inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs self;};
-      modules = [
-        self.nixosModules.default
-        "${nixosHostsDir}/${hostName}"
-      ];
-    };
+  # Import stable and unstable nixpkgs
+  pkgsStable = system: import inputs.nixpkgs-stable {inherit system;};
+  pkgsUnstable = system: import inputs.nixpkgs-unstable {inherit system;};
 
-  mkNixOnDroidHostConfig = hostName:
-    inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-      specialArgs = {inherit inputs self;};
-      modules = [
-        self.nixOnDroidModules.default
-        "${nixOnDroidHostsDir}/${hostName}"
-      ];
-    };
+  # Configuration builders for each platform
+  builders = {
+    nixos = mkNixosConfig;
+    nixOnDroid = mkNixOnDroidConfig;
+  };
 
-  nixosConfigurations = lib.genAttrs nixosHosts (host: mkNixosHostConfig host);
-  nixOnDroidConfigurations = lib.genAttrs nixOnDroidHosts (host: mkNixOnDroidHostConfig host);
+  # Helper functions for host discovery
+  getHostsForSystem = platformName: system:
+    pipe (builtins.readDir (platformDirs.${platformName} + "/${system}")) [
+      (filterAttrs (_: type: type == "directory"))
+      attrNames
+    ];
+
+  # Helper function to create a configuration for a single host
+  createHostConfiguration = platformName: system: host: {
+    name = host;
+    value = builders.${platformName} system host;
+  };
+
+  # Helper function to generate configurations for all hosts in a single system
+  createSystemConfigurations = platformName: system: let
+    systemPath = platformDirs.${platformName} + "/${system}";
+  in
+    if builtins.pathExists systemPath
+    then map (createHostConfiguration platformName system) (getHostsForSystem platformName system)
+    else [];
+
+  # Generate configurations for all systems and hosts for a given platform
+  makeConfigurations = platformName: let
+    systems = attrNames self.allSystems;
+    systemConfigurations = map (createSystemConfigurations platformName) systems;
+  in
+    listToAttrs (flatten systemConfigurations);
 in {
   flake = {
-    inherit nixosConfigurations nixOnDroidConfigurations;
+    nixosConfigurations = makeConfigurations "nixos";
+    nixOnDroidConfigurations = makeConfigurations "nixOnDroid";
   };
 }
