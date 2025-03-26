53f41a884142f85a212a70807d6740e3cb1defe6
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-03-26 18:59:32 +0530

    feat(system): Automate flake.lock updates and refine system auto-upgrade
    - Introduces a GitHub Actions workflow to automatically update the
    flake.lock file and merge the changes.
    - Modifies the system auto-upgrade module to use a direct flake
    reference and refresh the lockfile.

--- modules/nixos/system/update/default.nix
+++ modules/nixos/system/update/default.nix
@@ -1,11 +1,12 @@
 {
-  self,
   config,
   lib,
   ...
 }:
 with lib; let
   cfg = config.internal.system.autoUpdate;
+  host = config.networking.hostName;
+  input = "git+ssh://git@github.com/vipulog/nixos-config?ref=main";
 in {
   options.internal.system.autoUpdate = {
     enable = mkEnableOption "auto updates";
@@ -14,13 +15,12 @@ in {
   config = mkIf cfg.enable {
     system.autoUpgrade = {
       enable = true;
-      flake = self.outPath;
+      flake = "${input}#${host}";
       dates = "daily";
       randomizedDelaySec = "45min";
       operation = "boot";
       flags = [
-        "--update-input"
-        "nixpkgs"
+        "--refresh"
         "-L" # print build logs
       ];
     };
