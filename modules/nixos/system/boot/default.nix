91493cb9ca37567383da96ab32a72fde85bf29d1
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-03-28 08:08:35 +0530

    feat(boot): Increase systemd-boot configuration limit
    This commit increases the number of stored systemd-boot configurations.
    
    -   Sets `boot.loader.systemd-boot.configurationLimit` to 20, up from
    10.
    
    This allows the system to retain more previous boot configurations,
    providing more recovery options in case of issues with a new system
    version.

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -12,7 +12,7 @@ in {
 
   config = mkIf cfg.enable {
     boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.configurationLimit = 10;
+    boot.loader.systemd-boot.configurationLimit = 20;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.timeout = 3;
 
