17b6683f3b433cc733282732570226a199980bfa
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-05-03 16:35:07 +0530

    Support global and interface-specific rules

--- modules/nixos/services/networking/firewall/default.nix
+++ modules/nixos/services/networking/firewall/default.nix
@@ -7,29 +7,51 @@ with lib; let
   cfg = config.internal.services.networking.firewall;
 in {
   options.internal.services.networking.firewall = {
-    interface = mkOption {
-      type = types.str;
-      default = "";
-      description = "Interface to open ports on.";
-    };
+    enable = mkEnableOption "firewall rules";
 
-    udp = mkOption {
+    globalTCP = mkOption {
       type = types.listOf types.port;
       default = [];
-      description = "List of UDP ports to allow.";
+      description = "List of TCP ports to allow globally.";
     };
 
-    tcp = mkOption {
+    globalUDP = mkOption {
       type = types.listOf types.port;
       default = [];
-      description = "List of TCP ports to allow.";
+      description = "List of UDP ports to allow globally.";
+    };
+
+    interfaces = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          tcp = mkOption {
+            type = types.listOf types.port;
+            default = [];
+            description = "List of TCP ports to allow on this interface.";
+          };
+          udp = mkOption {
+            type = types.listOf types.port;
+            default = [];
+            description = "List of UDP ports to allow on this interface.";
+          };
+        };
+      });
+      default = {};
+      description = "Attribute set of interface-specific firewall rules.";
     };
   };
 
-  config = mkIf (cfg.interface != "") {
-    networking.firewall.interfaces.${cfg.interface} = {
-      allowedTCPPorts = cfg.tcp;
-      allowedUDPPorts = cfg.udp;
+  config = mkIf cfg.enable {
+    networking.firewall = {
+      allowedTCPPorts = cfg.globalTCP;
+      allowedUDPPorts = cfg.globalUDP;
     };
+
+    networking.firewall.interfaces =
+      mapAttrs (_name: cfg: {
+        allowedTCPPorts = cfg.tcp;
+        allowedUDPPorts = cfg.udp;
+      })
+      cfg.interfaces;
   };
 }
