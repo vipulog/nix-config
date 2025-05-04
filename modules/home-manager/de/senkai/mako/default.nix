c40a5559ec7cf1b12ffdc39a44a885efc414a392
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-05-04 14:44:53 +0530

    Update mako config

--- modules/home-manager/de/senkai/mako/default.nix
+++ modules/home-manager/de/senkai/mako/default.nix
@@ -9,6 +9,13 @@ in {
   config = mkIf senkaiCfg.enable {
     services.mako = {
       enable = true;
+      settings = {
+        font = mkForce "monospace 12";
+        margin = "8";
+        outer-margin = "6";
+        padding = "8";
+        width = "360";
+      };
     };
   };
 }
