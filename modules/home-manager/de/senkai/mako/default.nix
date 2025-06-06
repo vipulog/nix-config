5fa2fd0008699dd873c90b459d07666e2f7c194e
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-06-06 17:56:14 +0530

    update mako config

--- modules/home-manager/de/senkai/mako/default.nix
+++ modules/home-manager/de/senkai/mako/default.nix
@@ -5,16 +5,19 @@
 }:
 with lib; let
   senkaiCfg = config.internal.de.senkai;
+  colors = config.lib.stylix.colors.withHashtag;
 in {
   config = mkIf senkaiCfg.enable {
     services.mako = {
       enable = true;
       settings = {
         font = mkForce "monospace 12";
-        margin = "8";
-        outer-margin = "4";
+        margin = "4";
+        outer-margin = "0";
         padding = "8";
         width = "320";
+        border-color = mkForce colors.base03;
+        border-size = 3;
       };
     };
   };
