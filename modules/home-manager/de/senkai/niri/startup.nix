d7e1bd0a3a997fb0896de8e0cdf97aab51570a90
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-06-06 15:12:56 +0530

    update startup config

--- modules/home-manager/de/senkai/niri/startup.nix
+++ modules/home-manager/de/senkai/niri/startup.nix
@@ -3,19 +3,33 @@
   config,
   pkgs,
   self,
+  inputs',
   ...
 }:
 with lib; let
   senkaiCfg = config.internal.de.senkai;
 in {
   config = mkIf senkaiCfg.enable {
-    programs.niri.settings.spawn-at-startup = [
-      {command = ["${getExe pkgs.xwayland-satellite}"];}
-      {command = ["${getExe' pkgs.swayosd "swayosd-server"}"];}
-      {command = ["${getExe pkgs.clipse}" "-listen"];}
-      {command = ["${getExe pkgs.swaybg}" "-i" "${self.lib.relativeToRoot "assets/wallpapers/wallpaper.jpg"}"];}
-      # https://github.com/sodiboo/niri-flake/issues/509
-      {command = ["systemctl" "--user" "restart" "xdg-desktop-portal-gnome"];}
-    ];
+    programs.niri.settings.spawn-at-startup =
+      [
+        {command = ["${getExe pkgs.xwayland-satellite}"];}
+        {command = ["${getExe' pkgs.swayosd "swayosd-server"}"];}
+        {command = ["${getExe pkgs.clipse}" "-listen"];}
+        {command = ["${getExe pkgs.swaybg}" "-i" "${self.lib.relativeToRoot "assets/wallpapers/wallpaper.jpg"}"];}
+        # https://github.com/sodiboo/niri-flake/issues/509
+        {command = ["systemctl" "--user" "restart" "xdg-desktop-portal-gnome"];}
+      ]
+      ++ lib.optionals config.internal.programs.ghostty.enable [
+        {command = ["${getExe pkgs.ghostty}"];}
+      ]
+      ++ lib.optionals config.internal.programs.zen-browser.enable [
+        {command = ["${getExe inputs'.zen-browser.packages.default}"];}
+      ]
+      ++ lib.optionals config.internal.programs.whatsapp-web.enable [
+        {command = splitString " " "${config.xdg.desktopEntries.whatsapp-web.exec}";}
+      ]
+      ++ lib.optionals config.internal.programs.btop.enable [
+        {command = ["${getExe pkgs.ghostty}" "--command=${getExe pkgs.btop}" "--title=btop"];}
+      ];
   };
 }
