3d2d8763bb0295666a5aff3749eec5a3c3d49db6
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-06-11 09:49:14 +0530

    update ghostty + zellij config

--- modules/home-manager/programs/ghostty/default.nix
+++ modules/home-manager/programs/ghostty/default.nix
@@ -8,16 +8,60 @@ with lib; let
 in {
   options.internal.programs.ghostty = {
     enable = mkEnableOption "ghostty";
+
+    enableZellijIntegration = mkOption {
+      description = "Whether to enable zellij integration.";
+      default = config.internal.programs.zellij.enable;
+    };
   };
 
   config = mkIf cfg.enable {
     programs.ghostty = {
       enable = true;
-      settings = {
-        window-decoration = "server";
-        window-padding-x = 8;
-        window-padding-y = 8;
-      };
+
+      settings =
+        {
+          window-padding-balance = true;
+          window-padding-x = 3;
+          window-padding-y = 3;
+          window-padding-color = "background";
+        }
+        // lib.optionalAttrs cfg.enableZellijIntegration {
+          command = "zellij";
+          keybind = [
+            "super+ctrl+shift+plus=unbind"
+            "super+ctrl+shift+up=unbind"
+            "super+ctrl+shift+down=unbind"
+            "super+ctrl+shift+right=unbind"
+            "super+ctrl+shift+left=unbind"
+            "super+ctrl+left_bracket=unbind"
+            "super+ctrl+right_bracket=unbind"
+            "ctrl+alt+up=unbind"
+            "ctrl+alt+down=unbind"
+            "ctrl+alt+right=unbind"
+            "ctrl+alt+left=unbind"
+            "ctrl+shift+e=unbind"
+            "ctrl+shift+o=unbind"
+            "ctrl+shift+enter=unbind"
+            "ctrl+shift+t=unbind"
+            "ctrl+shift+w=unbind"
+            "ctrl+shift+right=unbind"
+            "ctrl+shift+left=unbind"
+            "ctrl+shift+tab=unbind"
+            "alt+one=unbind"
+            "alt+two=unbind"
+            "alt+three=unbind"
+            "alt+four=unbind"
+            "alt+five=unbind"
+            "alt+six=unbind"
+            "alt+seven=unbind"
+            "alt+eight=unbind"
+            "alt+nine=unbind"
+            "ctrl+page_up=unbind"
+            "ctrl+page_down=unbind"
+            "ctrl+tab=unbind"
+          ];
+        };
     };
   };
 }
--- modules/home-manager/programs/zellij/default.nix
+++ modules/home-manager/programs/zellij/default.nix
@@ -1,17 +1,14 @@
 {
+  pkgs,
   lib,
   config,
+  osConfig,
+  inputs',
   ...
 }:
 with lib; let
   cfg = config.internal.programs.zellij;
 in {
-  imports = [
-    ./config.nix
-    ./layout.nix
-    ./sesh.nix
-  ];
-
   options.internal.programs.zellij = {
     enable = mkEnableOption "zellij";
   };
@@ -20,5 +17,28 @@ in {
     programs.zellij = {
       enable = true;
     };
+
+    xdg.configFile."zellij/config.kdl".text = readFile ./config.kdl;
+
+    xdg.configFile."zellij/layouts/default.kdl".text = readFile (
+      pkgs.replaceVars ./layouts/default.kdl.in (with config.lib.stylix.colors.withHashtag; {
+        ZJSTATUS_PLUGIN_LOCATION = "file://${inputs'.zjstatus.packages.default}/bin/zjstatus.wasm";
+        TIMEZONE = osConfig.time.timeZone;
+        COLOR_BASE01 = base01;
+        COLOR_BASE02 = base02;
+        COLOR_BASE03 = base03;
+        COLOR_BASE04 = base04;
+        COLOR_BASE05 = base05;
+        COLOR_BASE06 = base06;
+        COLOR_BASE08 = base08;
+        COLOR_BASE09 = base09;
+        COLOR_BASE0A = base0A;
+        COLOR_BASE0B = base0B;
+        COLOR_BASE0C = base0C;
+        COLOR_BASE0D = base0D;
+        COLOR_BASE0E = base0E;
+        COLOR_BASE0F = base0F;
+      })
+    );
   };
 }
