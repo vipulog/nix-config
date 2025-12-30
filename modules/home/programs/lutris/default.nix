18493b9364e2e49d5e680274c35970c4f18ad4e1
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2025-12-30 14:36:08 +0530

    add lutris config

--- modules/home/programs/lutris/default.nix
+++ modules/home/programs/lutris/default.nix
@@ -12,6 +12,13 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    home.activation.deployLutrisConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir -p $HOME/.local/share/lutris
+      if [ ! -e "$HOME/.local/share/lutris/lutris.conf" ]; then
+        cp ${./lutris.conf} "$HOME/.local/share/lutris/lutris.conf"
+      fi
+    '';
+
     programs.lutris = {
       enable = true;
 
