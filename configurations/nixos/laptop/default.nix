02c4ea15461d25bd86ae22bd601aa0e00e8cfb68
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2026-03-03 11:29:21 +0530

    enable touch screen input in laptop

--- configurations/nixos/laptop/default.nix
+++ configurations/nixos/laptop/default.nix
@@ -25,9 +25,9 @@
       rootPath + "/configurations/home/vipul_laptop";
 
     # Disable touch screen input
-    services.udev.extraRules = ''
-      ATTRS{name}=="ELAN900C:00 04F3:25A2", ENV{ID_INPUT_TOUCHSCREEN}="0", ENV{ID_INPUT}="0"
-    '';
+    # services.udev.extraRules = ''
+    #   ATTRS{name}=="ELAN900C:00 04F3:25A2", ENV{ID_INPUT_TOUCHSCREEN}="0", ENV{ID_INPUT}="0"
+    # '';
 
     zramSwap = {
       enable = true;
