5ce803caac59a9dd9fb8cfe1bae5f4eb0238993d
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2026-05-15 16:42:02 +0530

    add git-hooks-nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ modules/pre-commit.nix
@@ -0,0 +1,32 @@
+{inputs, ...}: {
+  imports = [inputs.git-hooks-nix.flakeModule];
+
+  flake-file.inputs = {
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  perSystem = {
+    pre-commit.settings = {
+      default_stages = ["pre-commit"];
+
+      hooks = {
+        check-added-large-files.enable = true;
+        check-case-conflicts.enable = true;
+        check-executables-have-shebangs.enable = true;
+        check-shebang-scripts-are-executable.enable = false;
+        check-merge-conflicts.enable = true;
+        detect-private-keys.enable = true;
+        fix-byte-order-marker.enable = true;
+        mixed-line-endings.enable = true;
+        trim-trailing-whitespace.enable = true;
+        end-of-file-fixer.enable = true;
+        forbid-new-submodules.enable = true;
+        check-symlinks.enable = true;
+        treefmt.enable = true;
+      };
+    };
+  };
+}
