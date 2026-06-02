376269fc4d3b292da21daed05d68b7761a114ca7
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2026-06-02 16:50:55 +0530

    flatten flake attribute paths

--- modules/den/aspects/hosts/iceberg/vm-variant.nix
+++ modules/den/aspects/hosts/iceberg/vm-variant.nix
@@ -1,18 +1,16 @@
 {self, ...}: {
   den.aspects.iceberg = {
     nixos = {
-      virtualisation = {
-        vmVariantWithDisko = {
-          virtualisation = {
-            memorySize = 2048;
-            cores = 2;
-            graphics = true;
+      virtualisation.vmVariantWithDisko = {
+        virtualisation = {
+          memorySize = 2048;
+          cores = 2;
+          graphics = true;
 
-            qemu.options = [
-              "-device virtio-vga-gl"
-              "-display gtk,gl=on"
-            ];
-          };
+          qemu.options = [
+            "-device virtio-vga-gl"
+            "-display gtk,gl=on"
+          ];
         };
       };
     };
