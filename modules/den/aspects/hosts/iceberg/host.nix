526585e930e0909fc9f0f971e1148dbbdbd4df85
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2026-06-27 18:21:43 +0530

    refactor(den): replace provides with policies

--- modules/den/aspects/hosts/iceberg/host.nix
+++ modules/den/aspects/hosts/iceberg/host.nix
@@ -1,47 +1,60 @@
-{den, ...}: {
-  den.aspects.iceberg = {
-    includes = [
-      den.aspects.ephemeral-host
-      den.aspects.sops-nix
-      den.aspects.niri-de
-    ];
-
-    nixos = {
-      lib,
-      config,
-      ...
-    }: {
-      boot.loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
-      };
-
-      networking = {
-        useDHCP = lib.mkDefault true;
-        networkmanager.enable = true;
-      };
-
-      ephemeral-host = let
-        mainDiskCfg = config.disko.devices.disk.main;
-        subVols = mainDiskCfg.content.partitions.root.content.subvolumes;
-      in {
-        enable = true;
-        nixMountpoint = subVols.nix.mountpoint;
-        persistentMountpoint = subVols.persistent.mountpoint;
-      };
-
-      preservation.preserve.directories = [
-        "/var/lib/systemd"
-        "/var/lib/NetworkManager"
-
-        "/var/log"
+{
+  den,
+  lib,
+  ...
+}: {
+  den = {
+    aspects.iceberg = {
+      includes = [
+        den.aspects.ephemeral-host
+        den.aspects.sops-nix
+        den.aspects.niri-de
+        den.policies.iceberg-to-tux
       ];
 
-      system.stateVersion = "25.11";
+      nixos = {
+        lib,
+        config,
+        ...
+      }: {
+        boot.loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+        };
+
+        networking = {
+          useDHCP = lib.mkDefault true;
+          networkmanager.enable = true;
+        };
+
+        ephemeral-host = let
+          mainDiskCfg = config.disko.devices.disk.main;
+          subVols = mainDiskCfg.content.partitions.root.content.subvolumes;
+        in {
+          enable = true;
+          nixMountpoint = subVols.nix.mountpoint;
+          persistentMountpoint = subVols.persistent.mountpoint;
+        };
+
+        preservation.preserve.directories = [
+          "/var/lib/systemd"
+          "/var/lib/NetworkManager"
+
+          "/var/log"
+        ];
+
+        system.stateVersion = "25.11";
+      };
     };
 
-    provides = {
-      tux = {
+    policies.iceberg-to-tux = {
+      host,
+      user,
+      ...
+    }: let
+      guard = host.name == "iceberg" && user.name == "tux";
+    in
+      lib.optional guard (den.lib.policy.include {
         includes = [
           den.aspects.sops-nix
           den.aspects.niri-de
@@ -62,7 +75,6 @@
         homeManager = {
           home.stateVersion = "25.11";
         };
-      };
-    };
+      });
   };
 }
--- modules/den/aspects/hosts/igloo/host.nix
+++ modules/den/aspects/hosts/igloo/host.nix
@@ -1,43 +1,56 @@
-{den, ...}: {
-  den.aspects.igloo = {
-    includes = [
-      den.aspects.ephemeral-host
-      den.aspects.sops-nix
-      den.aspects.niri-de
-    ];
-
-    nixos = {config, ...}: {
-      boot.loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
-      };
-
-      hardware.bluetooth.enable = true;
-      networking.networkmanager.enable = true;
-      services.blueman.enable = true;
-
-      ephemeral-host = let
-        mainDiskCfg = config.disko.devices.disk.main;
-        subVols = mainDiskCfg.content.partitions.root.content.subvolumes;
-      in {
-        enable = true;
-        nixMountpoint = subVols.nix.mountpoint;
-        persistentMountpoint = subVols.persistent.mountpoint;
-      };
-
-      preservation.preserve.directories = [
-        "/var/lib/systemd"
-        "/var/lib/bluetooth"
-        "/var/lib/NetworkManager"
-
-        "/var/log"
+{
+  den,
+  lib,
+  ...
+}: {
+  den = {
+    aspects.igloo = {
+      includes = [
+        den.aspects.ephemeral-host
+        den.aspects.sops-nix
+        den.aspects.niri-de
+        den.policies.igloo-to-tux
       ];
 
-      system.stateVersion = "25.11";
+      nixos = {config, ...}: {
+        boot.loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+        };
+
+        hardware.bluetooth.enable = true;
+        networking.networkmanager.enable = true;
+        services.blueman.enable = true;
+
+        ephemeral-host = let
+          mainDiskCfg = config.disko.devices.disk.main;
+          subVols = mainDiskCfg.content.partitions.root.content.subvolumes;
+        in {
+          enable = true;
+          nixMountpoint = subVols.nix.mountpoint;
+          persistentMountpoint = subVols.persistent.mountpoint;
+        };
+
+        preservation.preserve.directories = [
+          "/var/lib/systemd"
+          "/var/lib/bluetooth"
+          "/var/lib/NetworkManager"
+
+          "/var/log"
+        ];
+
+        system.stateVersion = "25.11";
+      };
     };
 
-    provides = {
-      tux = {
+    policies.igloo-to-tux = {
+      host,
+      user,
+      ...
+    }: let
+      guard = host.name == "igloo" && user.name == "tux";
+    in
+      lib.optional guard (den.lib.policy.include {
         includes = [
           den.aspects.sops-nix
           den.aspects.niri-de
@@ -58,7 +71,6 @@
         homeManager = {
           home.stateVersion = "25.11";
         };
-      };
-    };
+      });
   };
 }
