c1645d089cf468e16aa41fe1f3c9b245b7b5f916
vipulog <90324465+vipulog@users.noreply.github.com>
2026-09-15 21:04:40 +0530

    remove redundant kde connect firewall config

--- modules/den/aspects/features/kdeconnect/default.nix
+++ modules/den/aspects/features/kdeconnect/default.nix
@@ -2,23 +2,10 @@
   den.aspects.kdeconnect = {user}: {
     nixos = {
       programs.kdeconnect.enable = true;
-
-      networking.firewall = rec {
-        allowedUDPPortRanges = allowedTCPPortRanges;
-
-        allowedTCPPortRanges = [
-          {
-            from = 1714;
-            to = 1764;
-          }
-        ];
-      };
     };
 
     homeManager = {
-      services.kdeconnect = {
-        enable = true;
-      };
+      services.kdeconnect.enable = true;
     };
 
     persist = {
